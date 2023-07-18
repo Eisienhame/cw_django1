634e6a0d1c734480e1acbe0854ef5fa81e75bcf5
Eisiehame <knurov24@gmail.com>
2023-07-18 10:06:52 +0200

    миграции

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from config.settings import STATIC_ROOT, STATIC_URL
 
 
 urlpatterns = [
@@ -26,4 +27,4 @@ urlpatterns = [
     path('', include(('main.urls', 'main'), namespace='main')),
     path('users/', include('users.urls', namespace='users')),
     path('blog/', include('blog.urls', namespace='blog')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
